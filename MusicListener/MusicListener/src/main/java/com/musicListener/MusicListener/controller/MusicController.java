a7510cd400a266b2ed7392e4dae89f32d8e10909
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2024-02-17 00:39:45 -0300

    Update MusicController.java

--- MusicListener/MusicListener/src/main/java/com/musicListener/MusicListener/controller/MusicController.java
+++ MusicListener/MusicListener/src/main/java/com/musicListener/MusicListener/controller/MusicController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/songs")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MusicController {
 
     private final MusicService musicService;
